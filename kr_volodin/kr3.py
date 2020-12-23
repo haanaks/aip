a7022b4e9c68c8037b837343347b8c791143bd98
haanaks <76121413+haanaks@users.noreply.github.com>
2020-12-23 22:44:44 +0300

    Add files via upload

--- kr_volodin/kr3.py
+++ kr_volodin/kr3.py
@@ -1,11 +1,17 @@
+import time
+import random
+
 xa =[]
 yx = []
-x = [int(i) for i in input('Введите координаты х: ').split()]
+x, y = [], []
+for i in range(2):
+    x.append(random.uniform(-10,10))
+    y.append(random.uniform(-10,10))
+a = random.uniform(-10,10)
 print('Координаты х: ', x)
-y = [int(i) for i in input('Введите координаты y: ').split()]
 print('Координаты y: ', y)
-a = float(input('Введите скаляр а: '))
 print('Скаляр: ', a)
+start_time = time.time()
 for elem_x in x:
     xa.append(elem_x * a)
 print('Вектор х, умноженные на скаляр: ', xa)
@@ -13,3 +19,5 @@ for elem in zip(y, xa):
     x_elem, y_elem = elem
     yx.append(x_elem + y_elem)
 print('Вектор у: ', yx)
+with open('time.txt', 'a') as f:
+    f.write(str(time.time() - start_time) + '\n')
--- kr_volodin/kr4.py
+++ kr_volodin/kr4.py
@@ -1,4 +1,6 @@
 import random
+import time
+
 x = 0
 y = 0
 z = 0
@@ -9,14 +11,8 @@ n = int(input('Число строк в матрице'))
 m = int(input('Число столбцов в матрице'))
 a = [[random.randint(1, 10) for j in range(m)] for i in range(n)]
 b = [[random.randint(1, 10) for j in range(m)] for i in range(n)]
-for i in range(n):
-    print(a[i])
-print()
 
-for i in range(n):
-    print(b[i])
-print()
-
+start_time = time.time()
 for j in range(0,m):
     for i in range(0,n):
         for z in range(0,n):
@@ -28,3 +24,5 @@ for j in range(0,m):
 
 for i in range(n):
     print(c[i])
+with open('time.txt', 'a') as f:
+    f.write(str(time.time() - start_time) + '\n')
