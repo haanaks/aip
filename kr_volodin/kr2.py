f7cdf0430aa4b282a3781a7d560bb3d5862dcd1a
haanaks <76121413+haanaks@users.noreply.github.com>
2020-12-23 13:52:15 +0300

    то что сделал на паре

--- kr_volodin/kr2.py
+++ kr_volodin/kr2.py
@@ -1,15 +1,60 @@
 import time
 import os
 
-with open('earth.md', 'r') as f:
+#with open('earth.md', 'r') as f:
 
-    while True:
-        line = f.readline()
-        while line != '```\n':
-            print(line, end='')
-            line = f.readline()
-        time.sleep(0.31)
-        print(50*'\n')
+    #while True:
+        #line = f.readline()
+        #for n in range(1,14):
+            #while line != '```\n' or line != '# n\n':
+                #print(line, end='')
+                #line = f.readline()
+            #time.sleep(0.31)
+        #print(50*'\n')
         #os.system('cls')
-        #print('\r',end='')
+            #print('\r',end='')
 
+line = 0
+
+
+
+#while True:
+    #line = input()
+    #if line.startswith('```') or line.startswith('\n'):
+        #print('\r',end='')
+        #continue
+    #else:
+        #break
+data = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        print('')
+    try:
+        while True:
+            if line.startswith('```') or line.startswith('#'):
+                print('\033c', end='')
+            elif line.startswith() != ('```') and line.startswith() != '#' and line.startswith() != ('\n'):
+                data.append(line)
+                print('\033c', end='')
+            else:
+                for l in data:
+                    print(l, end='')
+                print('\033c', end='')
+                time.sleep(0.1)
+                continue
+    except KeyboardInterrupt:
+        print('\nBye!')
+
+line = 0
+data = []
+while True:
+    line = input()
+    if line.startswith('```') or line.startswith('\n'):
+        print('\r',end='')
+        continue
+    else:
+        break
+while True
+    line input()
